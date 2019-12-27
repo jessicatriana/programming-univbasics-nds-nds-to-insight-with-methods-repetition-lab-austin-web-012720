bb6fd6a96a4ef08452467ad06c4d84fad8418e4d
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-27 01:58:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,8 +38,8 @@ def total_gross(directors_totals)
 total = 0
 index = 0
 
-while index < list_of_directors(directors_database).length do 
-  total += directors_totals(directors_database)[index][:name]
+while index < directors_totals(directors_database).length do 
+  total += directors_totals(directors_database)[index]
  
 index += 1
  end
